63eeb82af2ca1382a982d006d4d0303d91f4625a
Yelena Leri <yelenaleri@gmail.com>
2022-10-02 23:23:51 +0300

    hw 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const refs = {
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+  body: document.querySelector('body'),
+};
+refs.startBtn.addEventListener('click', startClick);
+refs.stopBtn.addEventListener('click', stopClick);
 
+let timerId = null;
+
+function startClick() {
+  refs.startBtn.disabled = true;
+  refs.stopBtn.disabled = false;
+  timerId = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function stopClick() {
+  clearTimeout(timerId);
+  refs.startBtn.disabled = false;
+  refs.stopBtn.disabled = true;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
